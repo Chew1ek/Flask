8630d74ee7d9032aeb2dfdfbaea627e49aa9b7c9
Chewlek <6rawl6a11@gmail.com>
2025-03-09 23:35:21 +0300

    carousel added

--- main.py
+++ main.py
@@ -278,5 +278,47 @@ def result(nickname, level, rating):
                     </html>'''
 
 
+@app.route('/carousel')
+def carousel():
+    return f'''<!doctype html>
+                        <html lang="ru">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet" 
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                            crossorigin="anonymous">
+                            <title>Колонизация</title>
+                          </head>
+                          <body>
+                            <div id="carouselExample" class="carousel slide">
+                                <div class="carousel-inner">
+                                  <div class="carousel-item active">
+                                    <img src="static/img/pei1.jpg" class="d-block w-100">
+                                  </div>
+                                  <div class="carousel-item">
+                                    <img src="static/img/pei2.jpg" class="d-block w-100">
+                                  </div>
+                                  <div class="carousel-item">
+                                    <img src="static/img/pei3.jpg" class="d-block w-100">
+                                  </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                  <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                  <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
+                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" 
+                            integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" 
+                            crossorigin="anonymous"></script>
+                          </body>
+                        </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
